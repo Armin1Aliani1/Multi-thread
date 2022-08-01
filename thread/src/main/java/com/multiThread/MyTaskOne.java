7f2d60c97447684ff80253832d7b89ea9a53215d
Dragon2623 <armin1.aliani1@gmail.com>
2022-08-01 16:11:49 +0430

    add MyTaskOne

---
+++ thread/src/main/java/com/multiThread/MyTaskOne.java
@@ -0,0 +1,9 @@
+package com.multiThread;
+
+public class MyTaskOne implements Runnable {
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
+        System.out.println("Inside method run MyTaskOne...");
+    }
+}
